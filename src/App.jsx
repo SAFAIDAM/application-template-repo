9828308801901308208802590439e7611b27e580
SAFA IDAM HAMED <safa.idamhamed2@gmail.com>
2024-01-14 15:04:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl text-center color-black">welcom to vite with Tailwindcss and React</h1>
+  </>
+  )
+}
+
+export default App
